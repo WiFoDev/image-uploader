eb2f0beaefd2b9f7343111d25a95de100e8b43d9
WiFoDev <lou81804@gmail.com>
2022-11-14 21:03:36 -0500

    add card element

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import type {NextPage} from "next";
 
 const Home: NextPage = () => {
-  return <section>Welcome</section>;
+  return (
+    <section className="w-[25rem] h-[29rem] rounded-2xl border-slate-400/50 border-[1px] shadow-slate-400/50 shadow-lg">
+      Hello
+    </section>
+  );
 };
 
 export default Home;
