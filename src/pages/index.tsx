6d279fbfd94f871738eab162ec01f0b98afcfc56
WiFoDev <lou81804@gmail.com>
2022-11-14 21:10:29 -0500

    add readme file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,12 @@ import type {NextPage} from "next";
 
 const Home: NextPage = () => {
   return (
-    <section className="w-[25rem] h-[29rem] rounded-2xl border-slate-400/50 border-[1px] shadow-slate-400/50 shadow-lg">
-      Hello
+    <section className="w-[25rem] h-[29rem] rounded-2xl border-slate-400/50 border-[1px] shadow-slate-400/50 shadow-lg p-8 flex flex-col items-center">
+      <h1>Upload your image</h1>
+      <p>File should be Jpeg, Png...</p>
+      <div>drag</div>
+      <p>Or</p>
+      <button>Choose a file</button>
     </section>
   );
 };
