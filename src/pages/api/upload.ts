01e9b1caa39deb6ba964f88f016c076e45a4ee39
WiFoDev <lou81804@gmail.com>
2022-11-15 20:37:28 -0500

    add upload endpoint implementation

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -2,11 +2,34 @@
 import type {NextApiRequest, NextApiResponse} from "next";
 
 import nc from "next-connect";
+import multer from "multer";
 
-const handler = nc<NextApiRequest, NextApiResponse>();
+const upload = multer({dest: "./public/images"}).single("image");
 
-handler.post((req, res) => {
-  return res.status(200).json("Hello World");
+const handler = nc<NextApiRequest, NextApiResponse>({
+  onError(error, req, res) {
+    res.status(501).json({
+      error: `Sorry something wrong happened! ${error.message}`,
+    });
+  },
+  onNoMatch(req, res) {
+    res.status(405).json({
+      error: "Method not allowed",
+      success: false,
+    });
+  },
+});
+
+handler.use(upload);
+
+type ExtendedRequest = {
+  file?: Express.Multer.File;
+};
+
+handler.post<ExtendedRequest>((req, res) => {
+  return res
+    .status(200)
+    .json({success: true, path: `${req.file?.originalname}`});
 });
 
 export default handler;
