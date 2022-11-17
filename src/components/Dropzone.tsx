46832b3a638a08f3328f24d7214de09e19550fae
WiFoDev <lou81804@gmail.com>
2022-11-17 17:57:32 -0500

    add drag active visuals

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -9,22 +9,28 @@ type DropzoneProps = {
 };
 
 export const Dropzone: FC<DropzoneProps> = ({onDrop}) => {
-  const {getRootProps, getInputProps, open} = useDropzone({
-    accept: {"image/*": [".png", ".gif", ".jpeg", ".jpg"]},
-    onDrop,
-  });
+  const {getRootProps, getInputProps, open, isDragActive} =
+    useDropzone({
+      accept: {"image/*": [".png", ".gif", ".jpeg", ".jpg"]},
+      onDrop,
+    });
 
   return (
     <>
       <div
         {...getRootProps()}
-        className="border-[2px] bg-[#F6F8FB] rounded-xl w-full h-[13.75rem] border-dashed border-[#97BEF4] flex flex-col items-center"
+        className="relative overflow-hidden border-[2px] bg-[#F6F8FB] rounded-xl w-full h-[13.75rem] border-dashed border-[#97BEF4] flex flex-col items-center"
       >
         <input {...getInputProps()} />
         <div className="my-9">
           <Image alt="Image representation" src={imagePic} />
         </div>
         <p className="text-xs">Drag & Drop your image here</p>
+        {isDragActive && (
+          <div className="absolute flex flex-col items-center justify-center w-full h-full text-lg font-semibold backdrop-blur-md bg-background/50">
+            Drop Here <span className="animate-bounce">↓</span>
+          </div>
+        )}
       </div>
       <p className="my-1 text-xs">Or</p>
       <button
